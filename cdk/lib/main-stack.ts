212774cb53053959caa55903885d39d5b12e69e6
Bart-15 <brat.tabusao@gmail.com>
2025-07-15 19:48:28 +0800

    feat: add s3 stack

--- cdk/lib/main-stack.ts
+++ cdk/lib/main-stack.ts
@@ -1,8 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { S3Stack } from "./s3-stack";
 
 export class MainStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new S3Stack(this, "S3Stack");
   }
 }
