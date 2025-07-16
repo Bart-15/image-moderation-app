b90090c9906c39332f4afc7182088b168e8f2296
Bart-15 <brat.tabusao@gmail.com>
2025-07-16 19:39:12 +0800

    feat: add api gateway in lambda-stack

---
+++ cdk/lib/api-gw-stack.ts
@@ -0,0 +1,38 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+export class ApiGatewayStack extends cdk.Stack {
+  constructor(
+    scope: Construct,
+    id: string,
+    props?: cdk.StackProps & { getPresignedUrlFunction: lambda.IFunction }
+  ) {
+    super(scope, id, props);
+
+    const api = new apigateway.RestApi(this, "ImageModerationApi", {
+      restApiName: "Image Moderation Service",
+      description:
+        "API for getting presigned URLs for image uploads and moderating images",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS, // TODO: Update this soon using correct environment
+        allowMethods: ["OPTIONS", "POST", "GET"],
+        allowHeaders: ["Content-Type"],
+      },
+    });
+
+    const presignedUrl = api.root.addResource("presigned-url");
+    presignedUrl.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(props?.getPresignedUrlFunction!, {
+        proxy: true,
+      })
+    );
+
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.url,
+      description: "API Gateway endpoint URL",
+    });
+  }
+}
--- cdk/lib/main-stack.ts
+++ cdk/lib/main-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { S3Stack } from "./s3-stack";
 import { LambdaStack } from "../lib/lambda-stack";
+import { ApiGatewayStack } from "./api-gw-stack";
 
 export class MainStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,6 +10,10 @@ export class MainStack extends cdk.Stack {
 
     const s3Stack = new S3Stack(this, "S3Stack");
 
-    new LambdaStack(this, "LambdaStack", { bucket: s3Stack.bucket });
+    const lambdaStack = new LambdaStack(this, "LambdaStack", { bucket: s3Stack.bucket });
+
+    new ApiGatewayStack(this, "ApiGatewayStack", {
+      getPresignedUrlFunction: lambdaStack.getPresignedUrlFunction
+    });
   }
 }
