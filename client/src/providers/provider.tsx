2294555c2b4bd8c2f7af4148f588b906b1320649
Bart-15 <brat.tabusao@gmail.com>
2025-07-24 20:11:08 +0800

    feat: setup tanstack query

---
+++ client/src/providers/provider.tsx
@@ -0,0 +1,18 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { queryClient } from "../config/queryClient";
+
+import "@aws-amplify/ui-react/styles.css";
+import "../lib/amplify";
+
+function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default Provider;
