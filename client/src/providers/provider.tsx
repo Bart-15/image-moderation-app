39865fb7ca0e0cbc6897cc41e176bce0d5efdb8e
Bart-15 <brat.tabusao@gmail.com>
2025-07-26 11:10:48 +0800

    feat(client): add sonner toast

--- client/src/providers/provider.tsx
+++ client/src/providers/provider.tsx
@@ -1,5 +1,6 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "sonner";
 
 import { queryClient } from "../config/queryClient";
 
@@ -11,6 +12,7 @@ function Provider({ children }: { children: React.ReactNode }) {
     <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster />
     </QueryClientProvider>
   );
 }
